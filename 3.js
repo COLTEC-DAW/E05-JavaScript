c8b103f0176d0de8e5dc502f60d880044fe4d5ea
Francisco Fabiano Neves <Francisco Fabiano Neves>
2017-04-24 17:55:15 -0300

    3 funcionando legal

---
+++ 3.js
@@ -0,0 +1,14 @@
+var entrada = prompt("Insira uma string para verificarmos se é um palindromo");
+var entradaPurificada;
+for (var i = 0; i<entrada.length; i++){
+    if (entrada[i] == ' '){
+        entrada[i] = entrada [i+1];
+        i--;
+        console.log("purificando...")
+    }
+}
+for (i = 0; i<entrada.length; i++){
+    entradaPurificada[i] = entrada[i].toLowerCase;
+    console.log("lowering...")
+}
+console.log("resultado: "+entradaPurificada)
