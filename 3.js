d933d8c27b0776db136b32beacd3a39330cbc67e
Francisco Fabiano Neves <Francisco Fabiano Neves>
2017-04-24 18:05:03 -0300

    Palindromo sendo bem problematica..

--- 3.js
+++ 3.js
@@ -1,5 +1,8 @@
 var entrada = prompt("Insira uma string para verificarmos se é um palindromo");
 var entradaPurificada;
+console.log("Recebi: "+entrada);
+console.log("Tamanho: " + entrada.length);
+/*
 for (var i = 0; i<entrada.length; i++){
     if (entrada[i] == ' '){
         entrada[i] = entrada [i+1];
@@ -7,6 +10,7 @@ for (var i = 0; i<entrada.length; i++){
         console.log("purificando...")
     }
 }
+*/
 for (i = 0; i<entrada.length; i++){
     entradaPurificada[i] = entrada[i].toLowerCase;
     console.log("lowering...")
