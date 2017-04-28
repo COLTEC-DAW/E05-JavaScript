715bbf93761179f2a2ba46b59f77473f545ac187
Mateuhslc <matheuslcoelho@yahoo.com.br>
2017-04-28 14:06:44 -0300

    Ordena p/ console feito

--- ordena.js
+++ ordena.js
@@ -1,25 +1,56 @@
-var a = [33, 103, 3, 726, 200, 984, 198, 764, 9];
+⁠⁠function bubble(array, func) {
+	for (var i = 0; i < array.length; i++) {
+		for (var j = 0; j < (array.length - i - 1); j++) {
+			if(func(array[j], array[j + 1])) {
+				var temp = array[j];
+				array[j] = array[j+1];
+				array[j+1] = temp;
+			}
+		}
+	}
 
-function bubbleSort(a)
-{
-    var mudado, temp;
-    do {
-        mudado = false;
-        for (var i=0; i < a.length-1; i++) {
-            if (a[i] > a[i+1]) {
-                temp = a[i];
-                a[i] = a[i+1];
-                a[i+1] = temp;
-                mudado = true;
-            }
-        }
-    } while (mudado);
+	return array;
 }
 
-function ordenaArray(array, func){
-    return func(array);
+function crescente(v1, v2) {
+	return v1 > v2;
 }
 
-ordenaArray(a, bubbleSort);
+function decrescente(v1, v2) {
+	return v1 < v2;
+}
 
-console.log(a);
+function crescenteImpar(v1, v2) {
+	if((v1 % 2 != 0 && v2 % 2 != 0) || (v1 % 2 == 0 && v2 % 2 == 0)) {
+		return v1 > v2;
+	}
+	else {
+		if(v1 % 2 != 0) {
+			return false;
+		}
+		else if(v2 % 2 != 0) {
+			return true;
+		}
+	}
+}
+
+function decrescentePar(v1, v2) {
+	if((v1 % 2 != 0 && v2 % 2 != 0) || (v1 % 2 == 0 && v2 % 2 == 0)) {
+		return v1 < v2;
+	}
+	else {
+		if(v1 % 2 == 0) {
+			return false;
+		}
+		else if(v2 % 2 == 0) {
+			return true;
+		}
+	}
+}
+
+var array = [1,2,3,4,5,6,7,8,9,10,11,121];
+
+console.log(bubble(array, crescente));
+console.log(bubble(array, decrescente));
+console.log(bubble(array, crescenteImpar));
+console.log(bubble(array, decrescentePar));
