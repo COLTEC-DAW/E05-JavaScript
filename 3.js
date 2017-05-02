a1f02b74c2c7f54a4806ca28288df39cd8cbecca
wendervitor <vitornogueira502@gmail.com>
2017-05-01 22:49:59 -0300

    to fazendo

---
+++ 3.js
@@ -0,0 +1,16 @@
+function inverte(palavra){
+    for(i=0;i<palavra.length;i++){
+        
+    }
+}
+
+var string = prompt("Digite sua String:");
+var stringInvertida = [];
+stringInvertida=string.split(" ");
+stringInvertida=stringInvertida.reverse();
+var tam = stringInvertida.length;
+//console.log(tam);
+for(i=0;i<tam;i++){
+    //console.log("ola");
+}
+console.log(stringInvertida);
