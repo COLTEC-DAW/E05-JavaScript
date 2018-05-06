c1d6cba57f1487f0c4a6212ee9d278e220182ece
gabriel MeiraII <gabrielveazey7@gmail.com>
2018-05-05 21:44:18 -0300

    Estilizando pagina (aparentemente inutil)

--- E05-JavaScript.js
+++ E05-JavaScript.js
@@ -1,10 +1,11 @@
-function Desenha_triangulo(a) {
+function Desenha_triangulo() {
+	var tri = document.getElementById('tri');
 
-	for(var i =0;i<=a;i++){
-          document.write("\n");
-  
+	for(var i =0;i<=tri.value;i++){  
 	    for(var x =0;x<i;x++){  
-	      document.write("#");    
-	    }  
+		  document.write("#");
+			}  
+			document.write("<br>");
 	  }
 }
+
