dc00459015d5764c716b7d95bc6370d5778c6b45
ritarez <ritarezborges@gmail.com>
2018-05-07 08:53:52 -0300

    Exercicio 4 pronto

--- Objetos & Arrays/ex4.js
+++ Objetos & Arrays/ex4.js
@@ -1,3 +1,15 @@
 function deepEquals(obj1, obj2){
+    var arr1 = Object.getOwnPropertyNames(obj1);
+    var arr2 = Object.getOwnPropertyNames(obj2);
 
+    if(arr1.length == arr2.length){
+        for(var i = 0; i < arr1.length; i++){
+            if(!obj2.hasOwnProperty(arr1[i]))
+                return false;
+        }
+        return true;
+    }
+    return false;
 }
+
+deepEquals({"Nome": "Rita"}, {"Nome": "João"});
