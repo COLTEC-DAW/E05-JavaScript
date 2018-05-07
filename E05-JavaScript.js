53cce3f31a2ed6087d62502c5f9f976c9c1f5d9a
gabriel MeiraII <gabrielveazey7@gmail.com>
2018-05-07 16:05:05 -0300

    Recursividade(???)

--- E05-JavaScript.js
+++ E05-JavaScript.js
@@ -72,3 +72,17 @@ function max(a,b){
 		else
 		return b;
 }
+
+function mod2(number){
+	if(number%2 === 0)
+	return true;
+			else
+			return false;
+}
+
+function mod(num, mod){
+	if(num%mod === 0)
+	return true;
+			else
+			return false;
+}
