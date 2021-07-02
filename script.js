e9d4a312f6a3a4019b81653d019dc0f8f6a6e52c
erikneves04 <erikrrn04@gmail.com>
2021-07-02 15:56:33 -0300

    Parte IV: Um programa diferente...

--- script.js
+++ script.js
@@ -37,3 +37,18 @@ function Palindromo(alvo){
 
     return EhPalindromo;
 }
+
+// Parte IV: Um programa diferente...
+function Anormal(){
+    for(var i=1;i<=100;i++){
+        if(i%3 == 0 && i%5 == 0){
+            console.log("FizzBuzz");
+        }else if(i%3 == 0){
+            console.log("Fizz");
+        }else if(i%5 == 0){
+            console.log("Buzz");
+        }else{
+            console.log(i);
+        }
+    }
+}
