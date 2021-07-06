86ec9ebdb38cca0b5d188452f941849ccc052d5d
AvaMelillo <avamelillo@gmail.com>
2021-07-06 17:56:43 -0300

    Tabuleiro de xadrez

--- exercicios.js
+++ exercicios.js
@@ -11,3 +11,20 @@ function piramide (num){
 
 piramide(6)
 
+function xadrez (num){
+
+    for (var i = 0; i < num; i++){
+        
+        for(var j = 0; j < num; j++){
+            if (j % 2 == i % 2) {
+                process.stdout.write('#');
+            }
+            else {
+                process.stdout.write(' ');
+            }
+        }
+        process.stdout.write('\n');
+    }
+}
+
+xadrez(3)
