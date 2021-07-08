6a5ec27b20b7f1f97e4da63cc3a72839f3439727
pedroping <2019951899@teiacoltec.org>
2021-07-08 15:47:13 -0300

    Mínimo e Máximo

---
+++ min_max.js
@@ -0,0 +1,18 @@
+function min(a,b){
+    if(a<b){
+        return a;
+    }else{
+        return b;
+    }
+
+}
+function max(a,b){
+    if(a>b){
+        return a;
+    }else{
+        return b;
+    }
+}
+
+console.log(min(3,6));
+console.log(max(3,6));
