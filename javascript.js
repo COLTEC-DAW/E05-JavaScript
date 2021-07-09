403c6400d331318d405c31f5bbf0d1400b48445c
IsaacVictorDS <84185656+IsaacVictorDS@users.noreply.github.com>
2021-07-09 19:01:56 -0300

    FizzBuzz

--- javascript.js
+++ javascript.js
@@ -40,3 +40,16 @@ function palindromo(palavra) {
  	else
   	console.log(falso);
 }
+
+//Um programa diferente...
+
+for (var i = 1; i <= 100; i++) {
+    if ((i % 3 == 0) & (i % 5 == 0))
+        console.log('FizzBuzz');
+    else if (i % 3 == 0)
+        console.log('Fizz');
+    else if (i % 5 == 0)
+        console.log('Buzz');
+    else
+    	console.log(i);
+}
