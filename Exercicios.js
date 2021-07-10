f57e75d7cf3990b79237333817c79a9fa2b5f125
MoleSama <73478225+MoleSama@users.noreply.github.com>
2021-07-10 16:25:51 -0300

    Programa Diferente

--- Exercicios.js
+++ Exercicios.js
@@ -49,3 +49,15 @@ function palindromo(){
 }
 
 palindromo();
+
+function programaDiferente(){
+
+    for (var i = 0; i < 100; i++){
+
+        if (i % 3 == 0 && i % 5 != 0) console.log("Fizz");
+        else if (i % 5 == 0 && i % 3 != 0) console.log("Buzz");
+        else if (i % 3 == 0 && i % 5 == 0) console.log("FizzBuzz");
+    }
+}
+
+programaDiferente();
