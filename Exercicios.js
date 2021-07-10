a027743cec8af0d805236e4d27c60ace765a3fe1
Raphael Luz <rlc.luz43@gmail.com>
2021-07-09 23:08:10 -0300

    Tabuleiro de Xadrez

--- Exercicios.js
+++ Exercicios.js
@@ -9,3 +9,21 @@ function triangulo(nLinhas){
         console.log(aux);
     }
 }
+
+// Tabuleiro de Xadrez
+
+function xadrez (num){
+
+    for (var i = 0; i < num; i++){
+
+        for(var j = 0; j < num; j++){
+            if (j % 2 == i % 2) {
+                console.log("#");
+            }
+            else {
+                console.log("");
+            }
+        }
+        console.log('\n');
+    }
+}
