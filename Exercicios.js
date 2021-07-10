8e92a0a697f2d103257a8658cddd5a74cd367341
MoleSama <73478225+MoleSama@users.noreply.github.com>
2021-07-10 16:27:45 -0300

    Mínimo e Máximo

--- Exercicios.js
+++ Exercicios.js
@@ -61,3 +61,10 @@ function programaDiferente(){
 }
 
 programaDiferente();
+
+function min(num1,num2){
+    return ((num1<num2) ? num1:num2);
+}
+function max(num1,num2){
+    return ((num1>num2) ? num1:num2);
+}  
