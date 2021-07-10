24c946c88ff242838f93bbd3f442525691d95dad
Raphael Luz <rlc.luz43@gmail.com>
2021-07-09 23:38:23 -0300

    Criptografia

--- Exercicios.js
+++ Exercicios.js
@@ -169,3 +169,30 @@ function ordencao(vetor, Modo){
 
     return vetor;
 }
+
+// Criptografia
+
+function CifraDeCesar(string) {
+
+    var strEncrypted = "";
+
+    for (var i = 0; i < string.length; i++) {
+        if (string[i] != "!" && string[i] != " " && string[i] != "." && string[i] != "?") {
+
+            if (string.charCodeAt(i) <= 77) {
+                strEncrypted += String.fromCharCode(string.charCodeAt(i) + (Math.random() * (26 - 1) + 1));
+            }
+            else {
+                strEncrypted += String.fromCharCode(string.charCodeAt(i) - (Math.random() * (26 - 1) + 1));
+            }
+
+        } else {
+            strEncrypted += string[i];
+        }
+    }
+    return strEncrypted;
+}
+
+function Encriptar(str, criterio) {
+    return criterio(str);
+}
