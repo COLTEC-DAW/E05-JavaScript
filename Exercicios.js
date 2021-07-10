d6858636ed93a9539e7498cfaee142a8ccecb6fc
Raphael Luz <rlc.luz43@gmail.com>
2021-07-09 23:16:31 -0300

    Um programa diferente ...

--- Exercicios.js
+++ Exercicios.js
@@ -44,3 +44,20 @@ function Palindromo(num) {
 
     return ehPalindromo;
 }
+
+// Um programa diferente ...
+
+function ContadorDiferenciado() {
+    for (var i = 0; i < 100; i++) {
+      if (i % 3 == 0 && i % 5 != 0) {
+        console.log("Fizz");
+      } else if (i % 5 == 0 && i % 3 != 0) {
+        console.log("Buzz");
+      } else if (i % 5 == 0 && i % 3 == 0) {
+        console.log("FizzBuzz");
+      } else {
+        console.log(i);
+      }
+    }
+  }
+  
