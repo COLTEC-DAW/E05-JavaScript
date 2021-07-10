c2f6689e8254470ebcd7cd6914de3386289183bf
MoleSama <73478225+MoleSama@users.noreply.github.com>
2021-07-10 15:44:36 -0300

    Tabuleiro de Xadrez

--- Exercicios.js
+++ Exercicios.js
@@ -13,3 +13,23 @@ function DesenhandoUmTriangulo(){
 }
 
 DesenhandoUmTriangulo();
+
+function xadrez (){
+
+    var num = prompt("Digite o tamanho do xadrez");
+
+    for (var i = 0; i < num; i++){
+
+        for(var j = 0; j < num; j++){
+            if (j % 2 == i % 2) {
+                console.log("#");
+            }
+            else {
+                console.log("");
+            }
+        }
+        console.log('\n');
+    }
+} 
+
+xadrez ();
