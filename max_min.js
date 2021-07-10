cb3f20942d3cd67f4ec7ba4968c836c629cffe7a
vitoriasrcosta <vitoriasrcosta@gmail.com>
2021-07-09 22:02:45 -0300

    Máximo e Mínimo

---
+++ max_min.js
@@ -0,0 +1,20 @@
+function max(valor1, valor2) {
+    if(valor1 > valor2) {
+        return valor1;
+    }
+
+    else{
+        return valor2;
+    }
+}
+
+function min(valor1, valor2){
+    if(valor1 < valor2) {
+        return valor1;
+    }
+
+    else{
+        return valor2;
+    }
+}
+
