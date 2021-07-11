4f5c5ecd4edec4941b9feea56b83ab4cd19a1819
Filipe Bison de Souza <55369148+fbison@users.noreply.github.com>
2021-07-11 17:28:33 -0300

    Verificando um número

--- script.js
+++ script.js
@@ -234,4 +234,33 @@ function encriptografar(str, criterio){
     return segredo;
 }
 
-console.log(encriptografar("oiiiiiiiiii tudo bem com vocs", cifraDeCesar));
+///console.log(encriptografar("oiiiiiiiiii tudo bem com vocs", cifraDeCesar));
+
+function eimpar(num){
+    return (num%2!=0)
+}
+function epar(num){
+    return (num%2==0)
+}
+function verifica (num, criterio){
+    return criterio(num);
+}
+/*Transformações em uma String
+Implemente uma função que faça transformações nos caracteres de uma string. A transformação a ser realizada deverá ser enviada por parâmetro, na forma de função.
+
+Teste a função passando como parâmetro as seguintes transformações:
+
+Caixa alta nas vogais
+Caixa alta nas consoantes
+Caixa baixa nas vogais
+Caixa baixa nas consoantes
+Funções Matriciais
+Implemente uma função que crie matrizes com base em uma função matricial com base nos índices da matriz. A função matricial deverá ser enviada por parâmetro, na forma de função.
+
+Teste o exercício passando como parâmetro as seguintes funções matriciais:
+
+m[i,j] = i + j
+[i,j] = i * j
+m[i,j] = i == j ? 1 : 0
+[i,j] = i^2/(j+1)
+m[i,j] = i > j ? 1 : (i < j ? 5 : 0)*/
