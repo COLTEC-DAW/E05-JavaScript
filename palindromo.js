a1073f00b2c50df43a0f623c0ed94f7507808f27
NakedSnakeE <luiz10gustavo.lima@gmail.com>
2021-07-10 23:46:24 -0300

    Atividade de palindromo

---
+++ palindromo.js
@@ -0,0 +1,14 @@
+var palindromo = prompt("(Palindromo) Digite uma palavra:");
+var aux=false;
+
+for(i = 0; i < palindromo.length / 2; i++){
+    if (palindromo[i] != palindromo[palindromo.length - i - 1]){
+        document.write(palindromo + " não é palindromo!");
+        aux=false;
+    }else{
+        aux=true;
+    }
+}
+if(aux==true){
+    document.write(palindromo + " é palindromo!");
+}
