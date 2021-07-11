ceed39fed8c57be80dffed18751ced75f8f6e61a
IagoDinizMartins <67806966+IagoDinizMartins@users.noreply.github.com>
2021-07-11 16:30:23 -0300

    Um programa diferente... completed

--- atividade.js
+++ atividade.js
@@ -53,3 +53,18 @@ var ehPalindrono = function  (str){
     }else{ return false; }
 }
 console.log(ehPalindrono("ava"))
+
+var fizzBuzzDe1a100 = function(){
+    for(var i = 1; i <= 100; i++){
+        if((i%3 == 0) && (i%5 == 0)){
+            console.log('FizzBuzz');
+        }else if(i%3 == 0){
+            console.log('Fizz');
+        }else if(i%5 == 0){
+            console.log('Buzz');
+        }else{
+            console.log(i)
+        }
+    }
+}
+fizzBuzzDe1a100();
