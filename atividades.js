4e64d239021de9e35b5f5af54588f9be54df46ad
BernardoVenancio <bernardovenancio1@gmail.com>
2021-07-11 12:32:59 -0300

    Tabuleiro de Xadrez

--- atividades.js
+++ atividades.js
@@ -11,3 +11,17 @@ function triangulo(nLinhas){
 }
 var nLinhas = prompt("Desenhando um triangulo - Quantas linhas terá o triangulo? ");
 triangulo(nLinhas);
+
+function xadrex(nLinhas){
+    console.log("Desenhando um tabuleiro");
+    var jogoDaVelha ="# # # #";
+    for (i = 0; i < nLinhas ; i++){
+        if(!(i % 2)){
+            console.log(jogoDaVelha)
+        }else{
+            console.log(" " + jogoDaVelha);
+        }
+    }
+}
+var nLinhas = prompt("Desenhando um tabuleiro - Quantas linhas terá o tabuleiro? ");
+xadrex(nLinhas);
