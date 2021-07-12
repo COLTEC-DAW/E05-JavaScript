a50c527b3f6db048d7bd7455a2c77f5a86c3469b
LeonardoZc <ventureleo@hotmail.com>
2021-07-11 21:46:01 -0300

    Palindromo

--- JavaS Ex05.js
+++ JavaS Ex05.js
@@ -15,6 +15,7 @@ triangulo(NLinhasT);
 /* Tabuleiro De Xadrez */
 var Xadrez = function(NLinhas){
     var HashTag = "# # # #";
+
     for (i = 0; i < NLinhas; i++){
         if(i % 2 == 0){
             console.log(HashTag);
@@ -27,3 +28,19 @@ var Xadrez = function(NLinhas){
 
 var NLinhasX = prompt("Quantas Linhas deseja que o tabuleiro de Xadrez tenha? ");
 Xadrez(NLinhasX);
+
+/* Palindromo */
+var Palindromo = function(Palavra){
+    var PalavraVerif = Palavra.split("").reverse().join("");
+
+    console.log(PalavraVerif);
+    if (Palavra.toLowerCase() === PalavraVerif.toLowerCase()){
+        console.log("%s É um palindromo", Palavra);
+    }
+    else{
+        console.log("%s Não é um palindromo", Palavra)
+    }
+}
+
+var Palavra = prompt("Digite a palavra que deseja conferir se é um palíndromo ")
+Palindromo(Palavra);
