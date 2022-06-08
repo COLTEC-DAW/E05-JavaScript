73a76a6f0d4189089a7a19ded9429cd04add279d
Amanda Bueno <amandabuenocampos@gmail.com>
2022-06-08 08:48:17 -0300

    Conceitos Basicos - Desenhando um triangulo

---
+++ E05.js
@@ -0,0 +1,44 @@
+/* Basic Concepts */
+
+/// triangle
+function printTriangle(numberOfRows){
+    for (let line = "##"; line.length < numberOfRows  + 2; line += "#") console.log(line);
+}
+
+/// chess board
+
+/// palindromes verifier 
+
+/// fizz buzz
+
+/* Functions */
+
+/// min & max
+
+/// recursion
+
+/// count chars
+
+/* Objects and Arrays */
+
+/// range -> max min 
+
+/// range -> max min i
+
+/// reverse array
+
+/// working with lists
+
+/// deep equals
+
+/* Higher-order Functions */
+
+/// sorting algorithms
+
+/// cryptography
+
+/// number verifier 
+
+/// transform string
+
+/// matrix functions
