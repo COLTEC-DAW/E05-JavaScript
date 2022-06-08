c91e2fc21b84b11f28f8d9e4676a72bf7a9ca405
Amanda Bueno <amandabuenocampos@gmail.com>
2022-06-08 10:05:19 -0300

    Conceitos Basicos - FizzBuzz

--- E05.js
+++ E05.js
@@ -20,7 +20,6 @@ function isPalindrome(justADummyString){
 }
 
 /// fizz buzz
-fizzBuzz();
 function fizzBuzz(){
     for(let i = 1; i <= 100; i++) console.log(i%3 == 0 ? (i%5 == 0 ? "FizzBuzz" : "Fizz") : (i%5 == 0 ? "Buzz" : i));
 }
