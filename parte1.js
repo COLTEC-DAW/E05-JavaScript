9738ee9e9f5915c4779da741bdf334da0b8e26b1
EnzoBraga <ebf2004@gmail.com>
2022-06-10 15:34:35 -0300

    Conceitos Básicos

--- parte1.js
+++ parte1.js
@@ -2,14 +2,14 @@
 
 function DesenhandoUmTriangulo() {
 
-    var tamanho = prompt("Digite o tamanho do triangulo:");
+    var tamanho = prompt(`Digite o tamanho do triangulo:`);
 
     for (var i = 0; i < tamanho; i++) {
 
         for (var j = 0; j <= i; j++) {
-            console.log("#");
+            console.log(`#`);
         }
-        console.log("\n");
+        console.log(`\n`);
     }
 }
 
@@ -19,17 +19,17 @@ DesenhandoUmTriangulo();
 
 function DesenhandoUmXadrez() {
 
-    var linhas = prompt("Digite a quantidade de linhas do xadrez:");
+    var linhas = prompt(`Digite a quantidade de linhas do xadrez:`);
 
     for (var i = 0; i < linhas; i++) {
 
         if (i % 2 == 0) {
-            console.log("# # # # ");
+            console.log(`# # # # `);
         }
         else {
-            console.log(" # # # #");
+            console.log(` # # # #`);
         }
-        console.log("\n");
+        console.log(`\n`);
     }
 }
 
@@ -39,7 +39,7 @@ DesenhandoUmXadrez();
 
 function VerificandoPalindromo() {
 
-    var palavra = prompt("Digite uma palavra ou uma frase");
+    var palavra = prompt(`Digite uma palavra ou uma frase`);
 
     palavra = palavra.toUpperCase();
     palavra = palavra.split(' ').join('');
@@ -47,14 +47,14 @@ function VerificandoPalindromo() {
     var palavraInvertida = palavra.split('').reverse().join('');
 
     if (palavra === palavraInvertida) {
-        console.log("É um palindromo");
+        console.log(`É um palindromo`);
     }
     else {
-        console.log("Não é um palindromo");
+        console.log(`Não é um palindromo`);
     }
 
-    console.log("A palavra/frase invertida:" + palavraInvertida);
-    console.log("A palavra/frase original:" + palavra);
+    console.log(`A palavra/frase invertida: ${palavraInvertida}`);
+    console.log(`A palavra/frase original:  ${palavra}`);
 }
 
 VerificandoPalindromo();
@@ -65,13 +65,13 @@ function FizzBuzz() {
 
     for (var i = 1; i <= 100; i++) {
         if (i % 3 == 0 && i % 5 == 0) {
-            console.log("FizzBuzz");
+            console.log(`FizzBuzz`);
         }
         else if (i % 3 == 0) {
-            console.log("Fizz");
+            console.log(`Fizz`);
         }
         else if (i % 5 == 0) {
-            console.log("Buzz");
+            console.log(`Buzz`);
         }
         else{
             console.log(i);
