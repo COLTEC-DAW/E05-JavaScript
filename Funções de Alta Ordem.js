3678d1115268cffa27dc345627c14bb0c1634115
Bigodeiro <enzobigber@gmail.com>
2022-06-10 10:25:16 -0300

    Verificação de número - FINALIZADA

--- Funções de Alta Ordem.js
+++ Funções de Alta Ordem.js
@@ -1,4 +1,5 @@
 //! FINALIZAR
+console.log("Ordenação\n")
 function bubbleSort(vet, criterio) 
 {
     concluido = 0
@@ -15,7 +16,10 @@ function bubbleSort(vet, criterio)
     
     // Teste a ordenação criando critérios de ordenação crescente, descrente, crescente ímpares, decrescente pares.    
 }
+console.log("\n")
 
+
+console.log("Encriptação\n")
 function crypt(frase, criterio)
 {
     vet = frase.split('');
@@ -34,18 +38,42 @@ function cifraCezar(caractere, chave)
     if (caractere.toUpperCase().charCodeAt(0) + chave <= 90) 
     {
         return String.fromCharCode(caractere.charCodeAt(0) + chave)
-    }else{return String.fromCharCode(caractere.charCodeAt(0) + chave - 26)}
+    }else return String.fromCharCode(caractere.charCodeAt(0) + chave - 26)
 }
 
-console.log("Texo encriptado: " + crypt('Enzozozozo', cifraCezar));
+texto = "Enzozozo"
+console.log("Texto a ser encriptado: " +  texto)
+console.log("Texo encriptado: " + crypt(texto, cifraCezar))
+console.log("\n")
 
 
+console.log("Verificação de número\n")
 
+function testaNum(num, condicao) {return condicao(num)}
 
-//! FINALIZAR
-function testaNum(condicao) {return condicao(num)}
+function ePrimo(num) 
+{
+    for (i = 2; i < num; i++)
+    {
+        if (num % i == 0) return false
+    }
+    return true
+}
+function eImpar(num) 
+{
+    if (num % 2 == 0) return false
+    else return true
+}
 
-num = 5
+num = 6
+
+//primo ou não
+if (testaNum(num, ePrimo)) console.log("o número " + num + " é primo")
+else console.log ("o número " + num + "não é primo")
+
+//pra ou ímpar
+if (testaNum(num, eImpar)) console.log("o número " + num + " é impar")
+else console.log("o número " + num + " é par")
 
 
 //!COMEÇAR
@@ -65,7 +93,7 @@ function stringTransform()
 //!COMEÇAR
 function criaMatriz()
 {
-    
+
 }
 // Funções Matriciais
 // Implemente uma função que crie matrizes com base em uma função matricial com base nos índices da matriz. A função matricial deverá ser enviada por parâmetro, na forma de função.
