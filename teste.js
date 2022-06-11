cd5792807d1a0113517b837724f761f29484ebcf
EnzoBraga <ebf2004@gmail.com>
2022-06-11 00:07:38 -0300

    pre delete do teste

--- teste.js
+++ teste.js
@@ -1,19 +1,57 @@
-function FizzBuzz() {
+function funcaoDeMatriz1(i, j) {
 
-    for (var i = 1; i <= 100; i++) {
-        if (i % 3 == 0 && i % 5 == 0) {
-            console.log("FizzBuzz");
-        }
-        else if (i % 3 == 0) {
-            console.log("Fizz");
-        }
-        else if (i % 5 == 0) {
-            console.log("Buzz");
-        }
-        else{
-            console.log(i);
-        }
-    }
+    var valor = i + j;
+    return valor;
 }
 
-FizzBuzz();
+function funcaoDeMatriz2(i, j) {
+
+    var valor = i * j;
+    return valor;
+}
+
+function funcaoDeMatriz3(i, j) {
+
+    var valor = i == j ? 1 : 0;
+    return valor;
+}
+
+function funcaoDeMatriz4(i, j) {
+
+    var valor = i ^ 2 / (j + 1);
+    return valor;
+}
+
+function funcaoDeMatriz5(i, j) {
+
+    var valor = i > j ? 1 : (i < j ? 5 : 0);
+    return valor;
+}
+
+matriz = [[0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0]]
+
+function criaMatriz(matriz, funcao) {
+
+    for (i = 0; i < matriz.length; i++) {
+        for (j = 0; j < matriz[0].length; j++) {
+            matriz[i][j] = funcao(i, j)
+        }
+        console.log(matriz[i]);
+    }
+    return matriz;
+}
+
+console.log(`Resultado da função 1`);
+matriz = criaMatriz(matriz, funcaoDeMatriz1);
+
+console.log(`Resultado da função 2`);
+matriz = criaMatriz(matriz, funcaoDeMatriz2);
+
+console.log(`Resultado da função 3`)
+matriz = criaMatriz(matriz, funcaoDeMatriz3);
+
+console.log(`Resultado da função 4`);
+matriz = criaMatriz(matriz, funcaoDeMatriz4);
+
+console.log(`Resultado da função 5`);
+matriz = criaMatriz(matriz, funcaoDeMatriz5);
