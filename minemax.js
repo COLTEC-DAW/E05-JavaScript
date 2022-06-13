672a014da4c73565eb58c9822c4d04875380e25d
Daniel Miranda <85716954+Dannillouou@users.noreply.github.com>
2022-06-13 16:20:20 -0300

    add minemax.js

---
+++ minemax.js
@@ -0,0 +1,17 @@
+function min(a, b){
+    if(a > b)
+        return b;
+    else if(a < b)
+        return a;
+    else
+        return 1;
+}
+
+function max(a, b){
+    if(a > b)
+        return a;
+    else if(a < b)
+        return b;
+    else
+        return 1;
+}
