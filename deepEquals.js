79cc87edf4468c58185446dec8d792bc3afc59a7
Daniel Miranda <85716954+Dannillouou@users.noreply.github.com>
2022-06-13 22:15:08 -0300

    Update deepEquals.js

--- deepEquals.js
+++ deepEquals.js
@@ -1,6 +1,12 @@
-function deepEquals(obj1, obj2){
-    if(obj1 === obj2)
-        return true;
-    else
-        return false;
+function deepEquals1(obj1, obj2){
+    let properties1 = Object.getOwnPropertyNames(obj1);
+    let properties2 = Object.getOwnPropertyNames(obj2);
+
+    let ehIgual = true;
+    properties1.forEach((i) => {
+        if(properties1[i] !== properties2[i])
+            ehIgual = false;
+    })
+
+    return ehIgual; 
 }
