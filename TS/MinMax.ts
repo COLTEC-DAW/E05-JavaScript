bdc10e392405cb402f73c71769e53310b0183bb7
CostaCesar <104566878+CostaCesar@users.noreply.github.com>
2023-04-18 08:10:10 -0300

    ADD: Atv2 - MinMax (TS only)

---
+++ TS/MinMax.ts
@@ -0,0 +1,15 @@
+function min(a: number, b: number): number
+{
+    if(a < b)
+        return a;
+    else return b;
+}
+function max(a: number, b: number): number
+{
+    if(a > b)
+        return a;
+    else return b;
+}
+
+console.log(min(13, 10));
+console.log(mxn(7, 24));
