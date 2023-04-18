8166c645a4b6cf7913ca9389c0212684ff26a7f9
CostaCesar <caioplays@outlook.com>
2023-04-17 22:51:17 -0300

    ADD: Atv1 - Triangle

---
+++ JS/Triangle.js
@@ -0,0 +1,7 @@
+var output = "#";
+var size = parseInt(prompt("Quantidade de linhas"));
+for (var i = 0; i < size; i++) {
+    output += "#";
+    console.log(output);
+}
+//# sourceMappingURL=Triangle.js.map
---
+++ TS/Triangle.ts
@@ -0,0 +1,8 @@
+let output : string = "#"
+let size : number = parseInt(prompt("Quantidade de linhas"));
+
+for(let i = 0; i < size; i++)
+{
+    output += "#";
+    console.log(output);
+}
