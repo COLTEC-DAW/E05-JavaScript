e44f665db6f15ab6ee48152a7c2c5376615d0432
CostaCesar <104566878+CostaCesar@users.noreply.github.com>
2023-04-19 15:32:06 -0300

    ADD: Atv3 - DeepEqual

---
+++ TS/DeepEqual.ts
@@ -0,0 +1,35 @@
+function compare(a: object, b: object): boolean
+{
+    if(typeof(a) != typeof(b))
+        return false;
+
+    let a_Keys = Object.keys(a);
+    let b_Keys = Object.keys(b);
+
+    for(let i = 0; i < a_Keys.length; i++)
+    {
+        if(a_Keys[i] != b_Keys[i])
+            return false;
+    }
+
+    let a_Vals = Object.values(a);
+    let b_Vals = Object.values(b);
+
+    for(let i = 0; i < a_Vals.length; i++)
+    {
+        if(a_Vals[i] != b_Vals[i])
+            return false;
+    }
+
+    return true;
+}
+
+let obj1 = {name: "Toby", age: 23};
+let obj2 = {name: "Clare", age: 24};
+let obj3 = {name: "Toby", age: 23};
+let obj4 = {bruh: "Toby", age: 23};
+
+console.log(compare(obj1, obj2));
+console.log(compare(obj1, obj3));
+console.log(compare(obj1, obj4));
+
