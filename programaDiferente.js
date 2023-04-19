b81edc35e3db162943959e281aa081fbfda545d0
lorenaricoy <lricoymartins@gmail.com>
2023-04-19 15:01:28 -0300

    up

--- programaDiferente.js
+++ programaDiferente.js
@@ -2,7 +2,7 @@ var x = "Fizz";
 var y = "Buzz";
 
 function imprimir (){
-    for (let i=1; i<101; i++){
+    for (let i=1; i<=100; i++){
         if (i%3==0 && i%5==0){
             console.log(x+y);
         } else { 
