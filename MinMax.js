4c1e2923a3c6b5e7b0c34283d9037be99d2d62c1
Leonardo <104566026+LeoPradoOliveira@users.noreply.github.com>
2023-04-19 13:59:49 -0300

    Create MinMax.js

---
+++ MinMax.js
@@ -0,0 +1,16 @@
+function min(a,b){
+    if(a>b)
+        return b;
+    else
+        return a;
+}
+
+function max(a,b){
+    if(a<b)
+        return b;
+    else
+        return a;
+}
+
+console.log(min(4, 7));
+console.log(max(8.1,2));
