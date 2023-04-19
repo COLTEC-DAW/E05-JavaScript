c6f9b1b7f2bd8821c397e2044fa67787ad3ed8df
Leonardo <104566026+LeoPradoOliveira@users.noreply.github.com>
2023-04-19 16:05:47 -0300

    Update intervalo.js

--- intervalo.js
+++ intervalo.js
@@ -1,10 +1,22 @@
 function range(min, max){
+    var array = [];
     for (let j = min + 1; j < max; j++) {
-        console.log(j);
+        array.push(j);
     }
+    return array; 
+}
+
+function range(min, max, i){
+    var array = [];
+    for (let j = min + 1; j < max; j += i) {
+        array.push(j);
+    }
+    return array; 
 }
 
 let a = parseInt(prompt("Min: "));
 let b = parseInt(prompt("Max: "));
+let c = parseInt(prompt("Intervalo de variação: "));
 
-range(a, b);
+console.log(range(a, b));
+console.log(range(a,b,c));
