687739501fd76c42798bb128c71acc2cd0dfcd7f
CostaCesar <104566878+CostaCesar@users.noreply.github.com>
2023-04-19 15:31:55 -0300

    ADD: Atv3 - List

---
+++ TS/List.ts
@@ -0,0 +1,36 @@
+class list
+{
+    value: number;
+    next: list | null;
+
+    constructor(data: number = 1234567890)
+    {
+        this.value = data;
+        this.next = null;
+    }
+}
+
+function toList(source: number[]): list
+{
+    let index: list = new list(source[0]);
+    let base: list = index;
+    for(let i = 1; i < source.length; i++)
+    {
+        let step: list = new list(source[i]);
+        index.next = step;
+        index = index.next;
+    }
+
+    console.log("Base: " + base.value);
+    return base;
+}
+
+function printList(input: list)
+{
+    for(let i: list | null = input; i != null; i = i.next)
+        console.log(i.value);
+}
+
+let arr: number[] = [1, 2 , 3, -4, 0];
+console.log(arr);
+printList(toList(arr));
