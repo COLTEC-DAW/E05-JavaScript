61e75ef4c20bee95b34b7b8e5bde2a0c1b303147
CostaCesar <caioplays@outlook.com>
2023-04-20 14:51:00 -0300

    FIX: Atv4 - Sorting - Using high-order funcs

--- JS/Sorting.js
+++ JS/Sorting.js
@@ -1,35 +1,36 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var SortTypes;
-(function (SortTypes) {
-    SortTypes[SortTypes["Increasing"] = 0] = "Increasing";
-    SortTypes[SortTypes["Decreasing"] = 1] = "Decreasing";
-})(SortTypes || (SortTypes = {}));
+let swap = (j, input) => {
+    let buffer = input[j + 1];
+    input[j + 1] = input[j];
+    input[j] = buffer;
+};
 function SortArray(input, order) {
-    let swap = (j) => {
-        buffer = input[j + 1];
-        input[j + 1] = input[j];
-        input[j] = buffer;
-    };
-    let buffer;
     for (let i = 0; i < input.length; i++) {
-        for (let j = 0; j < input.length - 1; j++) {
-            switch (order) {
-                case SortTypes.Increasing:
-                    if (input[j] > input[j + 1])
-                        swap(j);
-                    break;
-                case SortTypes.Decreasing:
-                    if (input[j] < input[j + 1])
-                        swap(j);
-                    break;
-            }
-        }
+        for (let j = 0; j < input.length - 1; j++)
+            order(input, j);
     }
     return input;
 }
 let toSort = [3, 45, 2, -16, 14, 13, 2, 4, 7, 10, -22];
 console.log(toSort);
-console.log(SortArray(toSort, SortTypes.Increasing));
-console.log(SortArray(toSort, SortTypes.Decreasing));
+console.log(SortArray(toSort, (input, index) => {
+    if (input[index] > input[index + 1])
+        swap(index, input);
+}));
+console.log(SortArray(toSort, (input, index) => {
+    if (input[index] % 2 == 0)
+        input.splice(index, 1);
+    else if (input[index] > input[index + 1])
+        swap(index, input);
+}));
+toSort = [3, 45, 2, -16, 14, 13, 2, 4, 7, 10, -22];
+console.log(SortArray(toSort, (input, index) => {
+    if (input[index] < input[index + 1])
+        swap(index, input);
+}));
+console.log(SortArray(toSort, (input, index) => {
+    if (input[index] % 2 != 0)
+        input.splice(index, 1);
+    else if (input[index] < input[index + 1])
+        swap(index, input);
+}));
 //# sourceMappingURL=Sorting.js.map
--- TS/Sorting.ts
+++ TS/Sorting.ts
@@ -1,58 +1,41 @@
-enum SortTypes{
-    Increasing,
-    Decreasing,
-    IncreasingOdd,
-    DecreasingEven,
+let swap = (j: number, input: number[]) => {
+    let buffer = input[j+1];
+    input[j+1] = input[j];
+    input[j] = buffer;
 }
 
-function SortArray(input: number[], order: SortTypes): number[]
+function SortArray(input: number[], order: Function): number[]
 { 
-    let swap = (j: number) => {
-        buffer = input[j+1];
-        input[j+1] = input[j];
-        input[j] = buffer;
-    }
-    
-    let buffer: number;
     for(let i = 0; i < input.length; i++)
     {
         for(let j = 0; j < input.length - 1; j++)
-        {
-            switch(order)
-            {
-                case SortTypes.Increasing:
-                    if(input[j] > input[j+1])
-                        swap(j)
-                    break;
-                case SortTypes.Decreasing:
-                    if(input[j] < input[j+1])
-                        swap(j)
-                    break;
-                case SortTypes.IncreasingOdd:
-                    if(input[j] % 2 == 0 )
-                        input.splice(j, 1);
-                    else if(input[j] > input[j+1])
-                        swap(j)
-                    break;
-                case SortTypes.DecreasingEven:
-                    if(input[j] % 2 != 0 )
-                        input.splice(j, 1);
-                    else if(input[j] < input[j+1])
-                        swap(j)
-                    break;
-            }
-        }
+            order(input, j);
     }
-    
     return input;
 }
 
 let toSort: number[] = [3, 45, 2, -16, 14, 13, 2, 4, 7, 10, -22];
 console.log(toSort);
 
-console.log(SortArray(toSort, SortTypes.Increasing))
-console.log(SortArray(toSort, SortTypes.IncreasingOdd))
+console.log(SortArray(toSort, (input, index) => {
+    if(input[index] > input[index+1])
+        swap(index, input)
+}));
+console.log(SortArray(toSort, (input, index) => {
+    if(input[index] % 2 == 0 )
+        input.splice(index, 1);
+    else if(input[index] > input[index+1])
+        swap(index, input)
+}));
 
 toSort = [3, 45, 2, -16, 14, 13, 2, 4, 7, 10, -22];
-console.log(SortArray(toSort, SortTypes.Decreasing))
-console.log(SortArray(toSort, SortTypes.DecreasingEven))
+console.log(SortArray(toSort, (input, index) => {
+    if(input[index] < input[index+1])
+        swap(index, input)
+}));
+console.log(SortArray(toSort, (input, index) => {
+    if(input[index] % 2 != 0 )
+        input.splice(index, 1);
+    else if(input[index] < input[index+1])
+        swap(index, input)
+}));
