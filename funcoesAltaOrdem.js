ebca25ceaab64aaacaae123655085f19601bd979
Júlia Luna <85693548+Julialunna@users.noreply.github.com>
2023-04-21 10:50:57 -0300

    criando função  tranformaString(string, transformacao) + funções para teste

--- funcoesAltaOrdem.js
+++ funcoesAltaOrdem.js
@@ -80,7 +80,74 @@ function ehPrimo(num){
     }
 }
 
+function tranformaString(string, transformacao){
+    var novaStr="";
+    for(let i=0;i<string.length;i++){
+        novaStr+=transformacao(string[i])
+    }
+    console.log(novaStr);
+}
+
+//funções para teste
+function vogalCaixaAlta(letra){
+    const vogais =["a","e","i", "o", "u", "A", "E", "I", "O", "U"];
+    var ehVogal=false;
+    for(let i=0;i<vogais.length;i++){
+        if(vogais[i]===letra){
+            ehVogal=true;
+        }
+    }
+    if(ehVogal){
+        return letra=letra.toUpperCase();
+    }else{
+        return letra=letra.toLowerCase();
+    }
+}
+function consoanteCaixaAlta(letra){
+    const vogais =["a","e","i", "o", "u", "A", "E", "I", "O", "U"];
+    var ehConsoante=true;
+    for(let i=0;i<vogais.length;i++){
+        if(vogais[i]===letra){
+            ehConsoante=false;
+        }
+    }
+    if(ehConsoante){
+        return letra=letra.toUpperCase();
+    }else{
+        return letra=letra.toLowerCase();
+    }
+}
+function vogalCaixaBaixa(letra){
+    const vogais =["a","e","i", "o", "u", "A", "E", "I", "O", "U"];
+    var ehVogal=false;
+    for(let i=0;i<vogais.length;i++){
+        if(vogais[i]===letra){
+            ehVogal=true;
+        }
+    }
+    if(ehVogal){
+        return letra=letra.toLowerCase();
+    }else{
+        return letra=letra.toUpperCase();
+    }
+}
+function consoanteCaixaBaixa(letra){
+    const vogais =["a","e","i", "o", "u", "A", "E", "I", "O", "U"];
+    var ehConsoante=true;
+    for(let i=0;i<vogais.length;i++){
+        if(vogais[i]===letra){
+            ehConsoante=false;
+        }
+    }
+    if(ehConsoante){
+        return letra=letra.toLowerCase();
+    }else{
+        return letra=letra.toUpperCase();
+    }
+}
+
 //testes
 ordenacao([1,3,4,5,2,6], decrescentePar);
 criptografia("Ola tudo Bem",cifraDeCesar);
 verificaNumero(7,ehPrimo);
+tranformaString("ola TUDO bem", vogalCaixaBaixa);
