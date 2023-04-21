a45cf6e351358887bd3253ec59b24c10d44d3327
Bernardo-O-Silva <2021951558@teiacoltec.org>
2023-04-21 21:35:41 +0000

    Resolução Exercício 1

--- Triangulo.js
+++ Triangulo.js
@@ -1,8 +1,8 @@
 var numLinhas = prompt("Digite o número de linhas: ");
 
-for(var comp = 1; comp <= numLinhas; comp++){
+for(let comp = 1; comp <= numLinhas; comp++){
 
-    for(var comp2 = 0; comp2 <= comp; comp2++){
+    for(let comp2 = 0; comp2 <= comp; comp2++){
 
         document.write("#");
 
---
+++ Xadrez.js
@@ -0,0 +1,11 @@
+var numLinhas = prompt("Digite o número de linhas: ");
+
+for(var comp = 1; comp <= numLinhas; comp++){
+
+    document.write("#");
+
+    
+
+    document.write("<br>");
+
+}
