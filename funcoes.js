186495a8c26288b54684463feff01c565b154626
Júlia Luna <85693548+Julialunna@users.noreply.github.com>
2023-04-21 10:22:02 -0300

    criando função mod(num, modulo)

--- funcoes.js
+++ funcoes.js
@@ -29,6 +29,18 @@ function mod2(number){
     }
 }
 
+function mod(num, modulo){
+    var resultado = num-modulo;
+    if(resultado>=modulo){
+        return mod(resultado, modulo);
+    }else if(resultado>=1&&resultado<=modulo-1){
+        return false;
+    }else if(resultado === 0){
+        return true
+    }
+}
+
 min(9,19);
 max(20,21);
 mod2(149);
+mod(81,8);
