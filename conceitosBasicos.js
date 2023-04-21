a60c50078bf0472f04fa36bf7cc5caca4a884a80
Júlia Luna <85693548+Julialunna@users.noreply.github.com>
2023-04-21 09:51:12 -0300

    criando função imprime_triangulo

--- conceitosBasicos.js
+++ conceitosBasicos.js
@@ -1,6 +1,22 @@
 function decide_exercicio(){
     var exercicio=prompt("Qual exercício deseja executar?");
     switch(exercicio){
-        
+    case 'triangulo':imprime_triangulo();
+    break;
     }
 }
+
+function  imprime_triangulo(){
+    const numLinhas = prompt("Digite a quantidade de linhas que deseja no triângulo");
+    var i=0, j=0;
+    var string="";
+    for(i=0;i<numLinhas;i++){
+        string="";
+        for(j=0;j<=i;j++){
+            string+="#";
+        }
+        console.log(string);
+    }
+}
+
+decide_exercicio();
