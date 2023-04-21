53275c9ad620bc3de74c394bc82c7d9eb3266915
Júlia Luna <85693548+Julialunna@users.noreply.github.com>
2023-04-21 09:53:50 -0300

    criando função imprime_jogo_xadrez

--- conceitosBasicos.js
+++ conceitosBasicos.js
@@ -3,6 +3,8 @@ function decide_exercicio(){
     switch(exercicio){
     case 'triangulo':imprime_triangulo();
     break;
+    case 'xadrez':imprime_jogo_xadrez();
+    break;
     }
 }
 
@@ -14,6 +16,27 @@ function  imprime_triangulo(){
         string="";
         for(j=0;j<=i;j++){
             string+="#";
+        }
+        console.log(string);
+    }
+}
+
+function imprime_jogo_xadrez(){
+    const numLinhas = prompt("Digite a quantidade de linhas que deseja no tabuleiro de xadrez");
+    var i=0, j=0;
+    var string="";
+    for(i=0;i<numLinhas;i++){
+        string="";
+        if(i%2===0){
+            for(j=0;j<4;j++){
+                string+="#";
+                string+=" ";
+            }
+        }else{
+            for(j=0;j<4;j++){
+                string+=" ";
+                string+="#";
+            }
         }
         console.log(string);
     }
