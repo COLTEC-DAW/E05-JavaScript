e3719e817ccd68a2d57468da5f518ea5c0c79b74
Bernardo-O-Silva <2021951558@teiacoltec.org>
2023-04-21 21:32:41 +0000

    Resolução do Exercício 1

---
+++ Triangulo.js
@@ -0,0 +1,13 @@
+var numLinhas = prompt("Digite o número de linhas: ");
+
+for(var comp = 1; comp <= numLinhas; comp++){
+
+    for(var comp2 = 0; comp2 <= comp; comp2++){
+
+        document.write("#");
+
+    }
+
+    document.write("<br>");
+
+}
