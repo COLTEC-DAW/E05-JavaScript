5f14587a20519a4bcb7ebb84ae1843ef40fad97c
Eduardo Birchal <104524053+EduardoBirchal@users.noreply.github.com>
2023-04-25 16:40:13 -0300

    terminei criptografia.js

--- criptografia.js
+++ criptografia.js
@@ -1,4 +1,6 @@
-const valorAMaiusculo = 65;
+const valorZMaiusculo = 90;
+const tamanhoAlfabeto = 26;
+const valorCifraCesar = 3;
 
 function fazCifra (string, cifra, stringCifrada = "", i = 0) {
     if (i < string.length) {
@@ -11,18 +13,19 @@ function fazCifra (string, cifra, stringCifrada = "", i = 0) {
 
 function cifraCesar (string, stringCifrada = "", i = 0) {
     if (i < string.length) {
-        let numChar = string.toUpperCase().charCodeAt(i) - valorAMaiusculo - 3;
-        let novoChar = 
+        let numChar = string.toUpperCase().charCodeAt(i);
 
-        numChar = string[i].toUpperCase().charCodeAt(0) - 65;
-        let novaString = stringCifrada.concat(cifra[numChar].toLowerCase());
-        return fazCifra(string, cifra, novaString, i+1);
+        if (numChar > valorZMaiusculo - valorCifraCesar) numChar -= tamanhoAlfabeto; // Se for Z, Y ou X, loopa pro início do alfabeto antes de terminar a operação
+
+        numChar += valorCifraCesar;
+        let novoChar = String.fromCharCode(numChar).toUpperCase();
+
+        let novaString = stringCifrada.concat(novoChar);
+        return cifraCesar(string, novaString, i+1);
     }
-    else return novaString;
-}
+    else return stringCifrada;
 }
 
 let string = prompt("Digite a string: ");
-let cifra = prompt("Digite a cifra: ");
 
-console.log("Sua string é: " + string + "\nSua string criptografada é: " + fazCifra(string, cifra));
+console.log("Sua string é: " + string + "\nSua string criptografada é: " + cifraCesar(string));
