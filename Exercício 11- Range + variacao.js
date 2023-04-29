7e873fcd4129ddefb2c5ad57a6e87788923ad566
Julia Leão <104568516+juliaaliceleao@users.noreply.github.com>
2023-04-29 14:36:51 -0300

    Add files via upload

---
+++ Exercício 11- Range + variacao.js
@@ -0,0 +1,18 @@
+var min = prompt("Digite o mínimo:");
+var max = prompt("Digite o maximo:");
+var variacao = prompt("Digite a variação:");
+
+function range(min, max, variacao)
+{
+    var intervalo = [];
+    var j = 0;
+
+    for(i = min; max >= i; i = i + variacao)
+    {
+        intervalo[j]= i;
+        j++;
+    }
+    return intervalo;
+}
+
+console.log(range(min,max,variacao));
